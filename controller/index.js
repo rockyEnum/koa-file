ccd6bf3116ca3dd7a094095a41af4c4d73b7e606
hanguoxin <hanguoxin>
2018-06-04 15:10:25 +0800

    增加判断img文件夹是否存在

--- controller/index.js
+++ controller/index.js
@@ -4,27 +4,39 @@ var util = require('util');
 var path = require('path')
 var basePath = path.dirname(require.main.filename);//获取主入口文件路径
 const formUpload = async (ctx) => {
-  let body = await saveFile(ctx)
- return body;
+    let body = await saveFile(ctx)
+    return body;
 }
-const saveFile = (ctx)=>{
+const saveFile = (ctx) => {
     return new Promise((resolve, reject) => {
         const form = new multiparty.Form();
         form.uploadDir = './img' //设置存放路径
         //fieids 存放其他表单数据
         //files 存放文件
-        form.parse(ctx.request.req, (err, fieids, files) => {
-            for (let item in files) {
-                var oldPath = basePath + '/' + files[item][0].path;
-                var newPath = basePath + '/img/' + files[item][0].originalFilename;
-                fs.rename(oldPath, newPath, (err) => {
-                    if (!err) {
-                        console.log('修改成功')
-                        resolve("修改成功")
-                    }
-                })
-            }
+        //判断文件路径是否存在
+        fs.stat(basePath+'/img',(err)=>{
+             if(err){
+               let mkdir = fs.mkdirSync(basePath+'/img')
+               saveImg(form,ctx,resolve,reject)
+             }else{
+                saveImg(form,ctx,resolve,reject)
+             }
         })
+       
+    })
+}
+const saveImg = (form,ctx,resolve,reject)=>{
+    form.parse(ctx.request.req, (err, fieids, files) => {
+        for (let item in files) {
+            var oldPath = basePath + '/' + files[item][0].path;
+            var newPath = basePath + '/img/' + files[item][0].originalFilename;
+            fs.rename(oldPath, newPath, (err) => {
+                if (!err) {
+                    console.log('修改成功')
+                    resolve("修改成功")
+                }
+            })
+        }
     })
 }
 module.exports = {
--- router/index.js
+++ router/index.js
@@ -1,6 +1,6 @@
 const controller = require('../controller/index')
 module.exports = (router) => {
-    //如果用到了koa-router插件，则中间件使用koa-router提供的写法去写，因为在 await next()执行的时候，走到路由后，发现异步执行后 接口还是响应404
+    //如果用到了koa-router插件，则中间件使用koa-router提供的写法去写，因为在 await next()执行走到路由后，发现异步执行后 接口还是响应404
     router.use(async (ctx, next) => {
         ctx.set("Access-Control-Allow-Origin", "*")
         await next();
